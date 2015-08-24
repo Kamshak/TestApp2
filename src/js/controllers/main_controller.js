e9090d9ddf3a04c0dec79ec52711823390796f56
Valentin Funk <funk.valentin@gmail.com>
2015-08-24 13:38:47 +0000

    y

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -1,29 +1,33 @@
 angular.module('Svenpanel.controllers.Main', ['fsCordova'])
 
-.controller('MainController', function($scope, CordovaService){
-  $scope.buttons = [
-      {text: "1,2,3 Gude Laune!", sound: "sound24.mp3"},
-      {text: "Hier Laune", sound: "sound25.mp3"},
-      {text: "Gude Laune", sound: "sound20.mp3"},
-      {text: "Laune OK", sound: "sound26.mp3"},
-      {text: "All about Laune", sound: "sound40.mp3"},
-    ];
-    
-    CordovaService.ready.then(function(){
-      for(var i = 0; i < $scope.buttons.length; i++) {
-        var src = "";
-        if (angular.isDefined(window.cordova) || true) {
-          
-          if (device.platform.toLowerCase() == 'android' || true) {
-            src = "/android_asset/www/audio/";
-          }
-          
-          src = src + $scope.buttons[i].sound;
-        }
-        
-        $scope.buttons[i].media = new Media(src, console.log.bind(console), console.error.bind(console));
-        console.log($scope.buttons[i].media);
-        
+.controller('MainController', function($scope, CordovaService) {
+  $scope.buttons = [{
+    text: "1,2,3 Gude Laune!",
+    sound: "sound24.mp3"
+  }, {
+    text: "Hier Laune",
+    sound: "sound25.mp3"
+  }, {
+    text: "Gude Laune",
+    sound: "sound20.mp3"
+  }, {
+    text: "Laune OK",
+    sound: "sound26.mp3"
+  }, {
+    text: "All about Laune",
+    sound: "sound40.mp3"
+  }, ];
+
+  CordovaService.ready.then(function() {
+    for (var i = 0; i < $scope.buttons.length; i++) {
+      var src = "";
+      if (device.platform.toLowerCase() == 'android') {
+        src = "/android_asset/www/audio/";
       }
-    });
+      src = src + $scope.buttons[i].sound;
+
+      $scope.buttons[i].media = new Media(src, console.log.bind(console), console.error.bind(console));
+      console.log($scope.buttons[i]);
+    }
+  }, console.log.bind(console));
 });
